439170ae08f5cf9d12d04fa491bb3a14bd702239
Bzaromedia <bzaromedia@gmail.com>
2021-04-09 06:00:59 -0400

    Learning Git. Forgive me for the crazy space.

--- flutter-apps/blog_api/routes/web.php
+++ flutter-apps/blog_api/routes/web.php
@@ -13,9 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/', 'App\Http\Controllers\BlogController@create');
 Route::get('/', 'App\Http\Controllers\DashboardController@index');
