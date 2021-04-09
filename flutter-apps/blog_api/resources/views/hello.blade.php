4940d10da1129343a4f9653561900a47a26df532
Bzaromedia <bzaromedia@gmail.com>
2021-04-09 03:23:44 -0400

    progressing along

--- flutter-apps/blog_api/resources/views/hello.blade.php
+++ flutter-apps/blog_api/resources/views/hello.blade.php
@@ -1,2 +1,10 @@
-<?php
-echo '<h1> Hello laravel developer</h1>';
+@extends('layout')
+@section('content')
+    <h3> From hello view</h3>
+    {{ $variable }} <br/>
+    {{ $blog->title }} <br/>
+
+    @foreach($array as $arr)
+        {{ $arr }}
+    @endforeach
+@stop
